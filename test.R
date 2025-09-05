6185b14f48e4782e70da7d3a1394602ccc82a2bd
寺井 雅人 <terai-masato@aut.ac.jp>
2025-09-05 16:03:02 +0900

    AD add color and labels

--- test.R
+++ test.R
@@ -1,3 +1,6 @@
 dat <- mtcars
 
-boxplot(mpg ~ cyl, data = dat)
+boxplot(mpg ~ cyl, data = dat,
+        col = "lightblue",
+        main = "MPG by Number of Cylinders",
+        xlab = "Cylinders", ylab = "Miles per Gallon")
